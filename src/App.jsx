d4e852c398f9bfe8c72b743c07f4332625a11540
daniel <122282403+danielma-tic@users.noreply.github.com>
2025-03-15 21:41:13 +0200

    Update App.jsx to import GameContext with .jsx extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ChainDrillMathGame from './components/ChainDrillMathGame';
-import GameProvider from './contexts/GameContext';
+import GameProvider from './contexts/GameContext.jsx';
 
 function App() {
   return (
