c29f61e09b22c582194dd14efb22cfc056ced940
daniel <122282403+danielma-tic@users.noreply.github.com>
2025-03-15 21:35:14 +0200

    Update App.jsx to wrap ChainDrillMathGame with GameProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ChainDrillMathGame from './components/ChainDrillMathGame';
+import GameProvider from './contexts/GameContext';
 
 function App() {
   return (
     <div className="App">
-      <ChainDrillMathGame />
+      <GameProvider>
+        <ChainDrillMathGame />
+      </GameProvider>
     </div>
   );
 }
