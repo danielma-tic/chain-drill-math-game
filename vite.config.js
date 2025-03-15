1ba0c1d02c1d4fc7ee8bca3917b51d7210268381
daniel <122282403+danielma-tic@users.noreply.github.com>
2025-03-15 21:38:33 +0200

    Update Vite config to handle JSX in .js files

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
+  esbuild: {
+    loader: {
+      '.js': 'jsx',
+    },
   },
-  build: {
-    outDir: 'dist',
-    minify: true
-  }
 });
